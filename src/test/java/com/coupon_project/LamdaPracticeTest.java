eaf276ebd5112def51b5502baecd1981edd03a46
jeongho <pjhpjh0805@naver.com>
2023-12-02 18:20:47 +0900

    코드 간소화

--- src/test/java/com/coupon_project/LamdaPracticeTest.java
+++ src/test/java/com/coupon_project/LamdaPracticeTest.java
@@ -1,13 +1,10 @@
 package com.coupon_project;
 
-import jdk.dynalink.Operation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
 import java.util.function.IntBinaryOperator;
 
 @SpringBootTest
@@ -16,11 +13,9 @@ public class LamdaPracticeTest {
     @Autowired
     LamdaPractice lamdaPractice;
 
-
     @Test
     void lamdaTest1() {
         IntBinaryOperator operator = Integer::sum;
-
         lamdaPractice.callMethod(operator, 1, 5 );
     }
 
@@ -39,6 +34,5 @@ public class LamdaPracticeTest {
             int result = method.applyAsInt(arg1, arg2);
             System.out.println("result = " + result);
         }
-
     }
 }
