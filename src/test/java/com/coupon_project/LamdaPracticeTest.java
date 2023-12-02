36a9482cc91c670e06f225df2a70b39182cffe4f
jeongho <pjhpjh0805@naver.com>
2023-12-02 18:13:36 +0900

    lamda 함수 연습

---
+++ src/test/java/com/coupon_project/LamdaPracticeTest.java
@@ -0,0 +1,44 @@
+package com.coupon_project;
+
+import jdk.dynalink.Operation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+import java.util.function.IntBinaryOperator;
+
+@SpringBootTest
+public class LamdaPracticeTest {
+
+    @Autowired
+    LamdaPractice lamdaPractice;
+
+
+    @Test
+    void lamdaTest1() {
+        IntBinaryOperator operator = Integer::sum;
+
+        lamdaPractice.callMethod(operator, 1, 5 );
+    }
+
+    @TestConfiguration
+    static class LamdaPracticeTestConfig {
+
+        @Bean
+        LamdaPractice lamdaPractice() {
+            return new LamdaPractice();
+        }
+    }
+
+    static class LamdaPractice {
+
+        public void callMethod(IntBinaryOperator method, int arg1, int arg2) {
+            int result = method.applyAsInt(arg1, arg2);
+            System.out.println("result = " + result);
+        }
+
+    }
+}
